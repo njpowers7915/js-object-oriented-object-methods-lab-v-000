0619e5e2ecb9ffe2cf8ce49247235e788849c068
Nicholas Powers <njpowers7915@gmail.com>
2018-08-01 15:10:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  
+  this.veto = function() {
+    console.log("No, I must disagree");
+  }
+  this.approve = function() {
+    console.log("You can do that!");
+  }
 }
