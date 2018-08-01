98ab4226dab850b64d083a090ac244c2860a1636
Nicholas Powers <njpowers7915@gmail.com>
2018-08-01 15:11:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function BoardMember(name, homeState, training) {
     return "No, I must disagree";
   }
   this.approve = function() {
-    console.log("You can do that!");
+    return "You can do that!";
   }
 }
